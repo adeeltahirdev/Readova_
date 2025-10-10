3a1f351c42e9ee0ea7f8018955404c197bcbdae7
adeeltahirdev <adeeltahir0301@gmail.com>
2025-10-10 21:46:24 +0500

    Readova Project

---
+++ src/router.jsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from "react-router";
+import IndexPage from "./pages/IndexPage";
+import BrowsePage from "./pages/BrowsePage";
+import LibraryPage from "./pages/LibraryPage";
+import RegisterPage from "./pages/auth/RegisterPage";
+import BorrowCheckout from "./pages/BorrowCheckout";
+import SubscribedCheckout from "./pages/SubscribedCheckout";
+import BookDetail from "./pages/BookDetail";
+import Pricing from "./pages/Pricing";
+import Admin from "./pages/auth/Admin";
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route index path="/" element={<IndexPage />} />
+      <Route path="/browse" element={<BrowsePage />} />
+      <Route path="/library" element={<LibraryPage />} />
+      <Route path="/auth/register" element={<RegisterPage />} />
+      <Route path="/borrowcheckout" element={<BorrowCheckout />} />
+      <Route path="/subscribedcheckout" element={<SubscribedCheckout />} />
+      <Route path="/bookdetail" element={<BookDetail />} />
+      <Route path="/pricing" element={<Pricing />} />
+      <Route path="/admin" element={<Admin />} />
+    </Routes>
+  );
+};
+
+export default Router;
