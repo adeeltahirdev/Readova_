a0c141894df1579af8c4e9ef4d5a7de6b52e03a1
Neasa Ali <hyderali11112@gmail.com>
2025-10-12 04:14:32 +0500

    api_updated

---
+++ Readova-backend/app/Models/Books.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Books extends Model
+{
+    protected $fillable = [
+        'google_id',
+        'title',
+        'authors',
+        'description',
+        'thumbnail',
+        'categories',
+        'published_date',
+        'info_link',
+    ];
+}
