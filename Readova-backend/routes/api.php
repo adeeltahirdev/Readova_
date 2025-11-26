6bc8f55143858743dfc3b38d933ba2eb8254b627
Neasa Ali <hyderali11112@gmail.com>
2025-11-26 06:21:28 +0500

    updation_completed

--- Readova-backend/routes/api.php
+++ Readova-backend/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\GoogleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\SubscriptionController;
 use App\Http\Controllers\UserPriceController;
 use PgSql\Lob;
 
@@ -23,4 +24,14 @@ Route::delete('deletebooks/{id}', [GoogleController::class, 'destroy']);
 Route::post('/rate', [GoogleController::class, 'rateBook']);
 Route::get('/books/{id}', [GoogleController::class, 'detail']);
 Route::post('/borrow', [GoogleController::class, 'borrowBook']);
-Route::post('/subscribeplan', [UserPriceController::class, 'store']);
+
+Route::get('/subscriptions/books', [SubscriptionController::class, 'getBooks']);
+Route::post('/subscriptions/checkout', [SubscriptionController::class, 'checkout']);
+
+Route::post('/wishlist/toggle', [GoogleController::class, 'toggleWishlist']);
+Route::get('/wishlist', [GoogleController::class, 'getWishlist']);
+Route::get('/wishlist/check/{id}', [GoogleController::class, 'checkWishlistStatus']);
+
+Route::get('/library/my-books', [GoogleController::class, 'getMyLibrary']);
+
+Route::get('/notifications', [GoogleController::class, 'getNotifications']);
